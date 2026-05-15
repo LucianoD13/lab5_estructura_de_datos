7a2f0de82b18e4f6b2718af30f8d03fc59274bef
Your Name <you@example.com>
2026-05-15 17:51:54 +0000

    new try

--- graph.c
+++ graph.c
@@ -58,6 +58,18 @@ int getWeight(Graph* g, const char* label1, const char* label2) {
     if (!g || !label1 || !label2) return -1;
 
     // Si no existe el origen o terminamos de iterar sin encontrar el destino
+    MapPair* p = map_search(g->adjancencyMap, (void*) label1);
+    if(p != NULL){
+        List* edges = p->value;
+        Edge* edge = list_first(edges);
+        while(edge != NULL){
+            if(strcmp(edge->target, label2) == 0){
+                return edge->weight;
+            }
+            edge = list_next(edges);
+        }
+        return -1;
+    }
     return -1; 
 }
 
